12b808f0c5c8ac03701d70bb47f6025658fbc7ca
Tony Lai <tony.lai@bounteous.com>
2024-01-19 15:17:00 -0500

    Force path to be absolute

--- scripts/aem.js
+++ scripts/aem.js
@@ -134,18 +134,18 @@ function sampleRUM(checkpoint, data = {}) {
 function setup() {
   window.hlx = window.hlx || {};
   window.hlx.RUM_MASK_URL = 'full';
-  window.hlx.codeBasePath = '';
+  window.hlx.codeBasePath = 'https://main--edge-commerce-integration--bounteouscanada.hlx.page';
   window.hlx.lighthouse = new URLSearchParams(window.location.search).get('lighthouse') === 'on';
 
-  const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
-  if (scriptEl) {
-    try {
-      [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error);
-    }
-  }
+  // const scriptEl = document.querySelector('script[src$="/scripts/scripts.js"]');
+  // if (scriptEl) {
+  //   try {
+  //     [window.hlx.codeBasePath] = new URL(scriptEl.src).pathname.split('/scripts/scripts.js');
+  //   } catch (error) {
+  //     // eslint-disable-next-line no-console
+  //     console.log(error);
+  //   }
+  // }
 }
 
 /**
